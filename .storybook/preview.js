3e1bc397c5b40ce744792b2a4e0561a646bad40b
João Gabriel <main.joaogabrielsg@gmail.com>
2023-03-08 18:45:55 -0300

    feat: add theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
-import GlobalStyles from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
   nextRouter: {
@@ -9,9 +11,9 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
