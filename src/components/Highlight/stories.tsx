6ccb4dea31d2e563adc509774bf91b524231c00c
João Gabriel <main.joaogabrielsg@gmail.com>
2023-03-19 16:24:58 -0300

    feat: create Highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Red Dead is back',
+    subtitle: "Come see John's new adventures",
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/rdr2'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
