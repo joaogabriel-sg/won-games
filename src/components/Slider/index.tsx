eb5f222ff05fecb1624d048312e34cf27df237ee
João Gabriel <main.joaogabrielsg@gmail.com>
2023-03-22 20:52:04 -0300

    feat: create Slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react'
+import SlickSlider, { Settings } from 'react-slick'
+
+import * as S from './styles'
+
+export type SliderSettings = Settings
+
+export type SliderProps = {
+  children: ReactNode
+  settings: SliderSettings
+}
+
+const Slider = ({ children, settings }: SliderProps) => (
+  <S.Wrapper>
+    <SlickSlider {...settings}>{children}</SlickSlider>
+  </S.Wrapper>
+)
+
+export default Slider
