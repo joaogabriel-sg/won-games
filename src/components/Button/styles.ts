f1fd910c62f7ff5eb18157e0f449daf11885b2fa
João Gabriel <main.joaogabrielsg@gmail.com>
2023-04-09 14:11:04 -0300

    feat: add disabled styles to Button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -40,11 +40,17 @@ const wrapperModifiers = {
     &:hover {
       color: ${darken(0.1, theme.colors.primary)};
     }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     display: inline-flex;
     align-items: center;
@@ -69,5 +75,7 @@ export const Wrapper = styled.button<WrapperProps>`
     ${!!hasIcon && wrapperModifiers.withIcon(theme)}
 
     ${!!minimal && wrapperModifiers.minimal(theme)}
+
+    ${!!disabled && wrapperModifiers.disabled()}
   `}
 `
