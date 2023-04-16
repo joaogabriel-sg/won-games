57a1e9e9f5f9f0e0190b572cfe8c8f5fc2ee08d6
João Gabriel <main.joaogabrielsg@gmail.com>
2023-04-16 15:14:28 -0300

    feat: add buy it now button to CartList component

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,19 +1,29 @@
 import GameItem, { GameItemProps } from 'components/GameItem'
 import * as S from './styles'
+import Button from 'components/Button'
+import Link from 'next/link'
 
 export type CartListProps = {
   items: GameItemProps[]
   total: string
+  hasButton?: boolean
 }
 
-const CartList = ({ items, total }: CartListProps) => (
+const CartList = ({ items, total, hasButton = false }: CartListProps) => (
   <S.Wrapper>
     {items.map((item) => (
       <GameItem key={item.title} {...item} />
     ))}
 
     <S.Footer>
-      Total <S.Total>{total}</S.Total>
+      {!hasButton && <span>Total:</span>}
+      <S.Total>{total}</S.Total>
+
+      {hasButton && (
+        <Link href="/cart" passHref>
+          <Button as="a">Buy it now</Button>
+        </Link>
+      )}
     </S.Footer>
   </S.Wrapper>
 )
