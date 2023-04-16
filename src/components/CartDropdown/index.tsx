d70f8695cf1952d1b0638389f5c2a2b8dd99a59f
João Gabriel <main.joaogabrielsg@gmail.com>
2023-04-16 15:25:30 -0300

    feat: create CartDropdown component

---
+++ src/components/CartDropdown/index.tsx
@@ -0,0 +1,21 @@
+import { GameItemProps } from 'components/GameItem'
+
+import * as S from './styles'
+import Dropdown from 'components/Dropdown'
+import CartIcon from 'components/CartIcon'
+import CartList from 'components/CartList'
+
+export type CartDropdownProps = {
+  items: GameItemProps[]
+  total: string
+}
+
+const CartDropdown = ({ items, total }: CartDropdownProps) => (
+  <S.Wrapper>
+    <Dropdown title={<CartIcon quantity={items.length} />}>
+      <CartList items={items} total={total} hasButton />
+    </Dropdown>
+  </S.Wrapper>
+)
+
+export default CartDropdown
