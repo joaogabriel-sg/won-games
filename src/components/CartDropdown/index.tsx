f364ea284b36df1ee20d658fbe2ecb84b2e91e66
João Gabriel <main.joaogabrielsg@gmail.com>
2023-04-16 15:35:19 -0300

    feat: show empty state in CartList component

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -6,11 +6,11 @@ import CartIcon from 'components/CartIcon'
 import CartList from 'components/CartList'
 
 export type CartDropdownProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
 }
 
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
+const CartDropdown = ({ items = [], total }: CartDropdownProps) => (
   <S.Wrapper>
     <Dropdown title={<CartIcon quantity={items.length} />}>
       <CartList items={items} total={total} hasButton />
