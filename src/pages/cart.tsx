2201ef04601ad8c42148d46283e01a6bfd1a9fa9
João Gabriel <main.joaogabrielsg@gmail.com>
2023-04-20 20:26:08 -0300

    feat: add query to get upcoming and recommended games to some pages

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,32 @@
 import { GetServerSideProps } from 'next'
 
 import Cart, { CartTemplateProps } from 'templates/Cart'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
 import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
+
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartTemplateProps) {
   return <Cart {...props} />
 }
 
-export const getServerSideProps: GetServerSideProps<
-  CartTemplateProps
-> = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
-      recommendedGames: gamesMock,
-      recommendedHighlight: highlightMock,
+      recommendedTitle: data.recommended?.section?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      ),
       items: itemsMock,
       total: 'R$ 430,00',
       cards: cardsMock
