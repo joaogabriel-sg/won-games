443ead099c7aba631a16f513b507642a574ce46f
João Gabriel <main.joaogabrielsg@gmail.com>
2023-04-25 20:03:23 -0300

    feat: create useCart and CartContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 
+import { CartProvider } from 'hooks/use-cart'
+
 import { useApollo } from 'utils/apollo'
 
 import GlobalStyles from 'styles/global'
@@ -14,19 +16,21 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#06092B" />
-          <meta
-            name="description"
-            content="The best Game Stores in the world!"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#06092B" />
+            <meta
+              name="description"
+              content="The best Game Stores in the world!"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   )
