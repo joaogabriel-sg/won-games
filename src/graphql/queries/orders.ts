7b5b36d973c7164e839c1cc045ce6f7a6f6adde2
João Gabriel <main.joaogabrielsg@gmail.com>
2023-05-02 20:58:27 -0300

    feat: add orders queries

---
+++ src/graphql/queries/orders.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client'
+import { GameFragment } from '../fragments/game'
+
+export const QUERY_ORDERS = gql`
+  query QueryOrders($identifier: ID!) {
+    orders(where: { user: { id: $identifier } }) {
+      id
+      created_at
+      card_brand
+      card_last4
+      games {
+        ...GameFragment
+      }
+    }
+  }
+
+  ${GameFragment}
+`
