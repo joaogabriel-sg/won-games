d226218cca62ab1e4d0e7e52915fe497f3406490
João Gabriel <main.joaogabrielsg@gmail.com>
2023-05-07 16:51:28 -0300

    test: add e2e tests to game page

---
+++ cypress/e2e/game.cy.ts
@@ -0,0 +1,63 @@
+describe('Game Page', () => {
+  beforeEach(() => {
+    cy.visit('/game/cyberpunk-2077')
+  })
+
+  it('should render game page sections', () => {
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+      cy.findByText(/^this game is part of your welcome offer!/i).should('exist')
+      cy.findByText('$99.98').should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
+
+    cy.getByDataCy('content').within(() => {
+      cy.findByRole('heading', { name: /description/i }).should('exist')
+    })
+
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publisher/i }).should('exist')
+      cy.findByRole('heading', { name: /rating/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+      cy.findAllByText(/cd projekt red/i).should('have.length', 2)
+      cy.findByText(/dec 8, 2020/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Role-playing / Action / Sci-fi').should('exist')
+    })
+
+    cy.shouldRenderShowcase({ name: 'Upcoming Games', highlight: true })
+    cy.shouldRenderShowcase({ name: 'You may like these games', highlight: false })
+  })
+
+  it('should add/remove game in cart', () => {
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i).first().should('have.text', 1).click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i).first().click()
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /remove from cart/i }).click()
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i).should('not.exist')
+  })
+})
--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -25,7 +25,7 @@ const CartList = ({ hasButton = false }: CartListProps) => {
   }
 
   return (
-    <S.Wrapper isEmpty={!items.length}>
+    <S.Wrapper isEmpty={!items.length} data-cy="cart-list">
       {items.length ? (
         <>
           <S.GamesList>
