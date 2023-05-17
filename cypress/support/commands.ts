778003b337c890c0bcc6b91949f80555b8defaa2
João Gabriel <main.joaogabrielsg@gmail.com>
2023-05-16 21:05:45 -0300

    chore: configure cypress-plugin-stripe-elements

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -26,6 +26,7 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 import '@testing-library/cypress/add-commands'
+import 'cypress-plugin-stripe-elements'
 
 Cypress.Commands.add('google', () => {
   cy.visit('https://google.com')
